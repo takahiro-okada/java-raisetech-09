d92ce49cb0eeb3a5daad7946bb4a8e93712e0a58
Takahiro Okada <dev_oka@Takahiros-MacBook-Pro.local>
2022-09-12 22:03:51 +0900

    first commit

---
+++ src/main/java/com/raisetech09/oka/OkaApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech09.oka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech09/oka/OkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech09.oka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
