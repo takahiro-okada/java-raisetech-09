3cc408a0962cf09f826e4dd760fedca1e8341a3c
Takahiro Okada <dev_oka@Takahiros-MacBook-Pro.local>
2022-09-13 11:30:46 +0900

    feat:index.htmlの削除

--- src/main/java/com/raisetech09/oka/controller/ProductController.java
+++ src/main/java/com/raisetech09/oka/controller/ProductController.java
@@ -2,8 +2,6 @@ package com.raisetech09.oka.controller;
 
 import com.raisetech09.oka.service.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.raisetech09.oka.repository.ProductMapper;
